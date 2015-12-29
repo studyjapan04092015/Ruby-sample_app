9f98be69495edf02c7453eae545f7e42b10e56b6
Dao Duy Nhan <StudyJapan04092015@gmail.com>
2015-12-29 16:12:23 +0700

    Fix error: why heroku cannot show /user/1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,17 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  def index
+  end
+
+  def edit
+  end
+
+  def create
+  end
+
+
   def new
   end
 end
