cffd87d73a1ac1c9c5894fc4a9826212dc2e25e8
Dao Duy Nhan <StudyJapan04092015@gmail.com>
2015-12-29 17:15:27 +0700

    Fix error: why heroku cannot show /user/1

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :users
+  resources :users
+  resources :users
   get 'users/new'
 
   get 'root_pages/home'
--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151228233122) do
+ActiveRecord::Schema.define(version: 20151229101251) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
-    t.string   "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
