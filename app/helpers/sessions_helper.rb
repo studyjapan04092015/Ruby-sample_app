ee4bfbcb148fdbc48155a6606f9e94956203ca58
Dao Duy Nhan <StudyJapan04092015@gmail.com>
2015-12-30 01:52:28 +0700

    Done edit profile, index users, destroy actions of Admin

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -11,13 +11,15 @@ module SessionsHelper
     cookies.permanent.signed[:user_id] = user.id
     cookies.permanent[:remember_token] = user.remember_token
   end
-
+  # Returns true if the given user is the current user.
+  def current_user?(user)
+    user == current_user
+  end
   # Returns the user corresponding to the remember token cookie.
   def current_user
     if (user_id = session[:user_id])
       @current_user ||= User.find_by(id: user_id)
     elsif (user_id = cookies.signed[:user_id])
-      raise
       user = User.find_by(id: user_id)
       if user && user.authenticated?(cookies[:remember_token])
         log_in user
@@ -36,8 +38,8 @@ module SessionsHelper
     session.delete(:user_id)
     @current_user = nil
   end
-
-  # Forgets a persistent session.
+  
+   # Forgets a persistent session.
   def forget(user)
     user.forget
     cookies.delete(:user_id)
@@ -50,5 +52,15 @@ module SessionsHelper
     session.delete(:user_id)
     @current_user = nil
   end
+  
+  # Redirects to stored location (or to the default).
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
 
+  # Stores the URL trying to be accessed.
+  def store_location
+    session[:forwarding_url] = request.url if request.get?
+  end
 end
---
+++ db/seeds.rb
@@ -0,0 +1,15 @@
+User.create!(name:  "Dao Duy Nhan",
+             email: "StudyJapan04092015@gmail.com",
+             password:              "123456hml",
+             password_confirmation: "123456hml",
+             admin: true)
+
+99.times do |n|
+  name  = Faker::Name.name
+  email = "example-#{n+1}@railstutorial.org"
+  password = "password"
+  User.create!(name:  name,
+               email: email,
+               password:              password,
+               password_confirmation: password)
+end
